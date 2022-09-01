8159eceb7566ceb38a9f6c751972b2a2d45eb3f3
flf2018 <1025601643@qq.com>
2022-09-01 15:40:59 +0800

    hashset 源码手写

---
+++ javaSE/HashSetTest.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+
+/**
+ * @desc Hashset 手写
+ * @author mypc
+ * @date 2022-09-01
+ *
+ */
+public class HashSetTest<E> {
+    private  HashMap map;
+
+    /**
+     * hashset 底层是hashmap
+     */
+    public HashSetTest() {
+        map = new HashMap<>();
+    }
+
+    /**
+     *  hsahset 中的元素值是map集合的key
+     *  所以不允许重复、可以为null
+     *  无序
+     * @param e
+     */
+    void add(E e){
+        map.put(e,new Object());
+    }
+
+    @Override
+    public String toString() {
+        return "HashSetTest{" +
+                "map=" + map +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        HashSetTest<String> set = new HashSetTest<>();
+        set.add("a");
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        set.add(null);
+        System.out.println(set.toString());
+    }
+}
